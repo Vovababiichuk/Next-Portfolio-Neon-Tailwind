66a391640c516565fbd3a34f6a9a8ed8807e4236
Vovababiichuk <babihuk888@gmail.com>
2024-01-01 16:57:46 +0200

    fix

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -9,10 +9,7 @@ const Projects = () => {
     <section id="projects">
       <MainTitle title='My Projects' subTitleType='projects' />
 
-      <div className="flex flex-col items-center justify-center py-20">
-        {/* <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
-          My Projects
-        </h1> */}
+      {/* <div className="flex flex-col items-center justify-center py-20">
         <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
           <ProjectCard
             src="/NextWebsite.png"
@@ -30,7 +27,7 @@ const Projects = () => {
             description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
           />
         </div>
-      </div>
+      </div> */}
     </section>
   );
 };
