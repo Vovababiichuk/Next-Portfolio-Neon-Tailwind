03281c4e79c5609fed168c4a33bf54674f6eaa64
Vovababiichuk <babihuk888@gmail.com>
2024-01-13 02:13:35 +0200

    update media for navbar

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -27,7 +27,7 @@ const links = [
 
 const Navbar = () => {
   return (
-    <div className="w-full h-[68px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-[1200] px-10">
+    <div className="w-full h-[68px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-[1200] px-4">
       <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px] max-xl:flex-wrap">
         {/* Logo */}
         <Link href="/" className="h-auto w-auto flex flex-row items-center">
@@ -36,20 +36,18 @@ const Navbar = () => {
             alt="logo"
             width={55}
             height={55}
-            className="cursor-pointer hover:animate-slowspin"
+            className="cursor-pointer hover:animate-slowspin mr-4"
           />
-        </Link>
-        <Link href="#home" className="h-auto w-auto flex flex-row items-center max-[530px]:hidden">
           <Image
             src="/nick.svg"
             alt="nick"
             width={280}
             height={280}
-            className="cursor-pointer hover:animate-slowspin"
+            className="cursor-pointer hover:animate-slowspin max-sm:hidden"
           />
         </Link>
         {/* Links */}
-        <nav className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20 max-lg:hidden">
+        <nav className="w-[490px] h-full flex flex-row items-center justify-between md:mr-20 max-lg:hidden">
           <ul className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200 text-[18px]">
             {links.map((link) => {
               return (
