f5f2e26520532cff81ade3d17e1b44cc53720022
Vovababiichuk <babihuk888@gmail.com>
2024-01-13 15:03:47 +0200

    fix video navbar media

--- components/main/HeroSection.tsx
+++ components/main/HeroSection.tsx
@@ -8,7 +8,7 @@ const HeroSection = () => {
         autoPlay
         muted
         // loop
-        className=" absolute top-[-690px] h-full w-full left-0 right-0 z-[1] object-cover">
+        className=" absolute top-[-690px] max-sm:top-[-626px] h-full w-full left-0 right-0 z-[1] object-cover">
         <source src="/video-header.mp4" type="video/mp4" />
       </video>
       <HeroContent />
