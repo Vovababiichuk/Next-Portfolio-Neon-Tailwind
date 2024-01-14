b606c65b1e16395b91bdbe1ee34b222801f525ac
Vovababiichuk <babihuk888@gmail.com>
2024-01-14 12:17:21 +0200

    commented meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
+      {/* <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover" />
-      </head>
+      </head> */}
       <body className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x:hidden`}>
           <StarsCanvas />
           <Navbar />
